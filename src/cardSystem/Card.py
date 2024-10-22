9f998c457cb6499a94ea75d384f0d0a5ddee312a
phukaokub <teerawat2545@gmail.com>
2024-10-22 14:43:28 +0700

    feat: render stats on card

--- src/cardSystem/Card.py
+++ src/cardSystem/Card.py
@@ -47,16 +47,29 @@ class Card:
         # Transparent card rendering at the bottom
         start_x = 80 + order * (CARD_WIDTH + 30)  # Space cards with 30px gap
         start_y = SCREEN_HEIGHT - CARD_HEIGHT - 10  # Place near the bottom
-        
-        # Draw the border around the card
-        pygame.draw.rect(screen, (0, 0, 0), (start_x, start_y, CARD_WIDTH, CARD_HEIGHT), 1)
 
         # Draw the card image
-
-        # Draw the card name
-        screen.blit(pygame.font.Font(None, 24).render(self.name, True, (255, 255, 255)), (start_x + 10, start_y + 10))
+        screen.blit(self.image, (start_x, start_y))
 
         # Draw card modified numbers
+        # Draw the speed value
+        font = pygame.font.Font(None, 24)
+        text = font.render(f'{self.speed}', True, (0, 0, 0))
+        screen.blit(text, (start_x + 170, start_y + 16))
+
+        # Draw the attack value
+        font = pygame.font.Font(None, 20)
+        text = font.render(f'{self.attack}', True, (0, 0, 0))
+        screen.blit(text, (start_x + 45, start_y + 212))
+
+        # Draw the range value
+        font2 = pygame.font.Font(None, 16)
+        text = font2.render(f'{self.range_start}-{self.range_end}', True, (0, 0, 0))
+        screen.blit(text, (start_x + 91, start_y + 213))
+
+        # Draw the defense value
+        text = font.render(f'{self.defense}', True, (0, 0, 0))
+        screen.blit(text, (start_x + 146, start_y + 212))
 
         # if selected
         if self.isSelected:
